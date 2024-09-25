7534306bbc115b7828849c83459c4137006dfcf4
Yanis Meichtry <yanis@meichtry.dev>
2024-09-25 16:28:16 +0200

    adding routes

--- index.ts
+++ index.ts
@@ -6,8 +6,21 @@ import figlet from 'figlet'
 const server = Bun.serve({
     port: 3000,
     fetch(req){
-        const body = figlet.textSync("Hola! senior?")
-        return new Response(body)
+        const url = new URL(req.url)
+        if(url.pathname == '/'){
+            const body = figlet.textSync("Hola! senior?")
+            return new Response(body)
+        }
+        
+        if(url.pathname === '/about'){
+            return new Response("About me!")
+        }
+
+        if(url.pathname === '/contact'){
+            return new Response("Contact me!")
+        }
+
+        return new Response('Not Found', {status: 404})
     }
 })
 
