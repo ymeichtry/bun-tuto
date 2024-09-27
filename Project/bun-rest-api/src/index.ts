d6f5f2de40080f9d5cdd18e859a2fa760b3c045f
Yanis Meichtry <yanis@meichtry.dev>
2024-09-27 11:51:28 +0200

    added .post and another .get

--- Project/bun-rest-api/src/index.ts
+++ Project/bun-rest-api/src/index.ts
@@ -3,6 +3,8 @@ import { Elysia } from "elysia";
 const app = new Elysia().get("/", () => "Hello Elysia")
 // .get('/post/:id', ({ params }: { params: { id: string } }) => { return { id: id, title: 'learn Bun' } })
 .get('/post/:id', ({params: {id}}) => {return {id: id, title: 'learn Bun'}})
+.post('/post', ({body}) => {return body})
+.get('/track/*', () => {return 'track all'})
 .listen(3000);
 
 console.log(
