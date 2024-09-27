fd5dd3429b975526a3f68468e2e08f33735cc2db
Yanis Meichtry <yanis@meichtry.dev>
2024-09-27 11:30:40 +0200

    made a get route

--- Project/bun-rest-api/src/index.ts
+++ Project/bun-rest-api/src/index.ts
@@ -1,6 +1,9 @@
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia")
+// .get('/post/:id', ({ params }: { params: { id: string } }) => { return { id: id, title: 'learn Bun' } })
+.get('/post/:id', ({params: {id}}) => {return {id: id, title: 'learn Bun'}})
+.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
